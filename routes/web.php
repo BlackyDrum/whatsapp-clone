44f1fea0c28e38d2db9b98e3ac04ee5f5dd45e83
BlackyDrum <ganiaytan1010@web.de>
2025-03-16 10:51:16 +0100

    Remove route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware('auth')->get('/', [HomeController::class, 'show'])->name('home');
-
 Route::middleware('auth')->group(function () {
     Route::get('/', [HomeController::class, 'show'])->name('home');
 
